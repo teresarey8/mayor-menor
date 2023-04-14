13803504151950f0f2b89c5ae2f8f3583628cf5a
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-14 08:48:15 +0200

    alert

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt("Introduzca aquí el primer número:"));
+let num2 = parseFloat(prompt("Introduzca aquí el segundo número"));
+
+if (num1>num2) {
+    alert("El primer número es mayor")
+}
+else if (num1<num2){
+    alert("El segundo número es mayor")
+}
