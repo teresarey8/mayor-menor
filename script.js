689cb8f380df67b641737d86c9f3c3df83a419d5
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-14 08:53:24 +0200

    impar

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ if (num1>num2) {
 else if (num1<num2){
     alert("El segundo número es mayor")
 }
+else if(num1==num2){
+    alert("Los dos números son iguales")
+}
+if (num2 % 2 == 0){
+    alert("El segundo número es par")
+}
+else{
+    alert("El segundo número es impar")
+}
